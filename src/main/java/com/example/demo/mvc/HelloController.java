faf328e1e44765119d76b72e81ecdd46980e138b
Alshatti0055 <alshatti0055@yahoo.com>
2025-07-12 22:15:48 +0200

    html with freemarker

---
+++ src/main/java/com/example/demo/mvc/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloPage(Model model) {
+        model.addAttribute("name", "Max Mustermann");
+        return "hello"; // wird hello.ftl unter templates aufrufen
+    }
+}
---
+++ src/main/resources/templates/hello.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>FreeMarker Beispiel</title>
+</head>
+<body>
+    <h1>Hallo ${name}!</h1>
+    <p>Willkommen auf deiner ersten FreeMarker-Seite mit Spring Boot.</p>
+</body>
+</html>
